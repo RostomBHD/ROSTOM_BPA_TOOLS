d4dacd7367fa0764408ca646e286611fe9bd816e
Rostom Benhamada <RostomBenhamada@aphnet.local>
2023-12-19 16:26:36 +0000

    database tools added

---
+++ ROSTOM_BPA_TOOLS/Input/CallStoredProcedure.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Data.Odbc;
+
+
+namespace ROSTOM_BPA_TOOLS.Input
+{
+    public class CallStoredProcedure
+    {
+        public DataTable ExecuteStoredProcedure(string connectionString, string procedureName, List<OdbcParameter> parameters)
+        {
+            if (parameters is null)
+            {
+                throw new ArgumentNullException(nameof(parameters));
+            }
+
+            using (OdbcConnection conn = new OdbcConnection(connectionString))
+            {
+                using (OdbcCommand cmd = new OdbcCommand(procedureName, conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    parameters.ForEach(param => cmd.Parameters.Add(param));
+
+                    OdbcDataAdapter adapter = new OdbcDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+    }
+}
---
+++ ROSTOM_BPA_TOOLS/Input/DatabaseQuery.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System.Data.Odbc;
+
+namespace ROSTOM_BPA_TOOLS.Input
+{
+    public class DatabaseQuery
+    {
+        public DataTable ExecuteQuery(string connectionString, string query)
+        {
+            using (OdbcConnection conn = new OdbcConnection(connectionString))
+            {
+                using (OdbcCommand cmd = new OdbcCommand(query, conn))
+                {
+                    conn.Open();
+                    OdbcDataAdapter adapter = new OdbcDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+    }
+}
